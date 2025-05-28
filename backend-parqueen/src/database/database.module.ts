090eb34f3ee3dff0048bdb286e8e1bc5e301d513
flammefolie <fguillemont@myges.fr>
2025-05-28 23:44:41 +0200

    fix: nest config

--- backend-parqueen/src/database/database.module.ts
+++ backend-parqueen/src/database/database.module.ts
@@ -1,22 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { User } from '../users/entities/user.entity';
 import { ParkingSpot } from '../parking-spots/entities/parking-spot.entity';
 import { Reservation } from '../reservations/entities/reservation.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port:  5432,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [User, Reservation, ParkingSpot], 
-      synchronize: true, 
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DATABASE_HOST'),
+        port: parseInt(config.get<string>('DB_PORT') || '5432', 10),
+        username: config.get<string>('POSTGRES_USER'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_DB'),
+        entities: [User, Reservation, ParkingSpot],
+        synchronize: true,
+      }),
     }),
   ],
 })
 
-export class DatabaseModule {} 
+export class DatabaseModule {
+}
