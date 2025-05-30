f73c9489430df4be97fa825f0cd39c23f4f5a403
badis <badis.tighlit@gmail.com>
2025-05-30 21:17:00 +0200

    feat(reservation): add business constraints and error handling

--- backend-parqueen/src/parking/reservations/services/reservations.service.ts
+++ backend-parqueen/src/parking/reservations/services/reservations.service.ts
@@ -8,6 +8,7 @@ import { User } from '../../../users/entities/user.entity';
 import { ParkingSpot } from '../../domain/model/parking-spot.entity';
 import { addDays, format, isAfter, isToday, isWeekend, parseISO } from 'date-fns';
 import { ParkingSpotsService } from '../../parking-spots/services/parking-spots.service';
+import { In } from 'typeorm';
 
 @Injectable()
 export class ReservationsService {
@@ -30,78 +31,102 @@ export class ReservationsService {
     return this.reservationRepo.find();
   }
 
+async create(dto: CreateReservationDto, userId:string): Promise<Reservation[]> {
+  const {  startDate, endDate, isElectric } = dto;
 
-  async create(dto: CreateReservationDto, userId: string): Promise<Reservation[]> {
-    const { startDate, endDate, isElectric } = dto;
+  const user = await this.userRepository.findOne({
+    where: { id: userId },
+    relations: ['reservations'],
+  });
 
-    const user = await this.userRepository.findOne({
-      where: { id: userId },
-      relations: ['reservations'],
-    });
-
-    if (!user) {
-      throw new NotFoundException(`Utilisateur avec id ${userId} non trouvé`);
-    }
-
-    const start = parseISO(startDate);
-    const end = parseISO(endDate);
-    const now = new Date();
-    const reservations: Reservation[] = [];
-
-    const MAX_RESERVATIONS = {
-      user: 5,
-      manager: 30,
-      admin: 30,
-    };
-
-    const maxAllowed = MAX_RESERVATIONS[user.role];
-
-    const existingFutureReservations = await this.reservationRepo.count({
-      where: {
-        user: { id: userId },
-        date: MoreThanOrEqual(format(now, 'yyyy-MM-dd')),
-      },
-    });
-
-
-    //découpage des dates
-    const newReservationDates: string[] = [];
-    for (let current = start; !isAfter(current, end); current = addDays(current, 1)) {
-      if (!isWeekend(current)) {
-        newReservationDates.push(format(current, 'yyyy-MM-dd'));
-      }
-    }
-
-    const totalRequest = newReservationDates.length;
-    const restingQuota = maxAllowed - existingFutureReservations;
-
-    if (totalRequest > restingQuota) {
-      throw new BadRequestException(
-        `Vous avez déjà ${existingFutureReservations} réservations à venir. Vous ne pouvez réserver que ${restingQuota} jours supplémentaire.`,
-      );
-    }
-
-    for (const dateStr of newReservationDates) {
-      const availableSpot = await this.parkingSpotsService.getAvailableSpotForDate(isElectric, dateStr);
-      if (!availableSpot) {
-        throw new NotFoundException(`Aucune place disponible pour le ${dateStr}`);
-      }
-
-      const currentDate = parseISO(dateStr);
-      const checkedIn = isToday(currentDate) && now.getHours() >= 11;
-
-      const reservation = this.reservationRepo.create({
-        user,
-        parkingSpot: availableSpot,
-        date: dateStr,
-        checkedIn,
-      });
-
-      reservations.push(reservation);
-    }
-
-    return this.reservationRepo.save(reservations);
+  if (!user) {
+    throw new NotFoundException(`Utilisateur avec id ${userId} non trouvé`);
   }
+
+  const start = parseISO(startDate);
+  const end = parseISO(endDate);
+  const now = new Date();
+  const reservations: Reservation[] = [];
+
+  const MAX_RESERVATIONS = {
+    user: 5,
+    manager: 30,
+    admin: 30,
+  };
+
+  const maxAllowed = MAX_RESERVATIONS[user.role];
+
+  const existingFutureReservations = await this.reservationRepo.count({
+    where: {
+      user: { id: userId },
+      date: MoreThanOrEqual(format(now, 'yyyy-MM-dd')),
+    },
+  });
+
+
+  //découpage des dates
+  const newReservationDates: string[] = [];
+  for (let current = start; !isAfter(current, end); current = addDays(current, 1)) {
+    if (!isWeekend(current)) {
+      newReservationDates.push(format(current, 'yyyy-MM-dd'));
+    }
+  }
+
+  const totalRequest = newReservationDates.length;
+  const restingQuota = maxAllowed - existingFutureReservations;
+
+  if (restingQuota==0) {
+    throw new BadRequestException(
+      `Vous avez déjà atteint le nombre maximum de réservations possibles : ${existingFutureReservations} . Vous ne pouvez pas faire de nouvelles réservations pour le moment.`
+    );
+  }
+
+  if (totalRequest > restingQuota) {
+    throw new BadRequestException(
+      `Vous avez déjà ${existingFutureReservations} réservations à venir. Vous ne pouvez réserver que ${restingQuota} jours supplémentaire.`
+    );
+  }
+
+    
+  const existingDates = await this.reservationRepo.find({
+    where: {
+      user: { id: userId },
+      date: In(newReservationDates),
+    },
+    select: ['date'],
+  });
+
+  if (existingDates.length > 0) {
+    const conflictDates = existingDates.map(r => r.date).join(', ');
+    throw new BadRequestException(
+      `Vous avez déjà une réservation pour les dates suivantes : ${conflictDates}`
+    );
+  }
+
+
+  for (const dateStr of newReservationDates) {
+    const availableSpot = await this.parkingSpotsService.getAvailableSpotForDate(isElectric, dateStr);
+    if (!availableSpot) {
+      throw new NotFoundException(`Aucune place disponible pour le ${dateStr}`);
+    }
+
+    const currentDate = parseISO(dateStr);
+    const checkedIn = isToday(currentDate) && now.getHours() >= 11;
+
+    const reservation = this.reservationRepo.create({
+      user,
+      parkingSpot: availableSpot,
+      date: dateStr,
+      checkedIn,
+    });
+
+    reservations.push(reservation);
+  }
+
+  return this.reservationRepo.save(reservations);
+}
+
+  
 
 
   async update(id: string, dto: UpdateReservationDto): Promise<Reservation> {
