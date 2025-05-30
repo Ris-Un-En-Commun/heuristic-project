c67b77312964d11681e59c8271040c19178c237f
MarcMalha <marc.malha@beanstock.com>
2025-05-31 00:32:57 +0200

    Feat: add route for getting the current users unchecked reservation if available

--- backend-parqueen/src/users/users.controller.ts
+++ backend-parqueen/src/users/users.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Post, Body,Get } from '@nestjs/common';
+import {Controller, Post, Body, Get, Req, NotFoundException, UseInterceptors} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import {Reservation} from "../parking/domain/model/reservation.entity";
+import {GetReservationForCheckInUseCase} from "../parking/application/get-reservation-for-check-in-use-case";
+import {RoleUserInterceptor} from "../common/interceptors/role-user.interceptor";
+
 @Controller('users')
+@UseInterceptors(RoleUserInterceptor)
 export class UsersController {
 
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+      private readonly usersService: UsersService,
+      private readonly checkInUseCase: GetReservationForCheckInUseCase,
+  ) {}
 
   @Get('Test')
     ping() {
@@ -17,5 +25,14 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @Get('/current-unchecked-reservation')
+  async getCurrentUncheckedReservation(@Req() request: Request): Promise<Reservation> {
+    const reservation  = await this.checkInUseCase.execute(request['user']?.id);
 
+    if (!reservation) {
+      throw new NotFoundException('Pas de reservation pour aujourd\'hui');
+    }
+
+    return reservation;
+  }
 }
--- backend-parqueen/src/users/users.module.ts
+++ backend-parqueen/src/users/users.module.ts
@@ -1,13 +1,23 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import {ParkingModule} from "../parking/parking.module";
+import {RESERVATION_REPO} from "../parking/infrastruture/tokens";
+import {ReservationRepo} from "../parking/infrastruture/reservation-repo";
+import {GetReservationForCheckInUseCase} from "../parking/application/get-reservation-for-check-in-use-case";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])], 
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => ParkingModule)],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+      UsersService,
+      ReservationRepo,
+      { provide: RESERVATION_REPO, useExisting: ReservationRepo },
+
+      GetReservationForCheckInUseCase
+  ],
   exports: [UsersService, TypeOrmModule], 
 })
 export class UsersModule {}
