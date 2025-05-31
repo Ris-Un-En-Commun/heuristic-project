96be6f74e0afd6b5f1ef5439543979223ccfcd74
flammefolie <fguillemont@myges.fr>
2025-05-31 16:41:54 +0200

    feat: (front) Fix bug

--- frontend-parqueen/src/components/common/Header.tsx
+++ frontend-parqueen/src/components/common/Header.tsx
@@ -7,7 +7,7 @@ import CheckInButton from "../reservation/check_in_button.tsx";
 const Header = () => {
     const {setRole, role} = useRole()
     return (
-        <header className="flex justify-center items-center bg-blue-500 py-2">
+        <header className="flex justify-center items-center gap-x-2 bg-blue-500 py-2">
             <CheckInButton role={role}></CheckInButton>
             <h1 className="font-bold text-white text-3xl">Parqueen</h1>
             <div className="justify-self-end">
